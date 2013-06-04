d39b9baecf16e7477be01ef35f385ae38d7e23db
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-06-04 12:04:23 +0200

    Back to development

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires = [
 ]
 
 setup(name='maxclient',
-      version='3.3',
+      version='3.4dev',
       description='Client library wrapper to access MAX API.',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[

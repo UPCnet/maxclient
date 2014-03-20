1cfaa9d488c5b350f99b3c541a2650edd9a48575
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-03-20 13:53:43 +0100

    Default for contexts

--- maxclient/defaults.py
+++ maxclient/defaults.py
@@ -1,3 +1,4 @@
 ENDPOINT_METHOD_DEFAULTS = {
-    '/people/{username}/activities_post': {'object': {'objectType': 'note'}}
+    '/people/{username}/activities_post': {'object': {'objectType': 'note'}},
+    '/contexts_post': {'objectType': 'context'}
 }

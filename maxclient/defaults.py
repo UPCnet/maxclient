0fc14c443218b79d6e98607083e0c278284f5745
Carles Bruguera <carlesba@gmail.com>
2014-06-24 22:07:22 +0200

    Update defaults

--- maxclient/defaults.py
+++ maxclient/defaults.py
@@ -3,5 +3,6 @@ ENDPOINT_METHOD_DEFAULTS = {
     '/people/{username}/conversations/{id}/messages_post': {'object': {'objectType': 'note'}},
     '/contexts/{hash}/activities_post': {'object': {'objectType': 'note'}},
     '/contexts_post': {'objectType': 'context'},
-    '/people/{username}/subscriptions_post': {'object': {'objectType': 'context'}}
+    '/people/{username}/subscriptions_post': {'object': {'objectType': 'context'}},
+    '/conversations_post': {"object": {'objectType': 'note'}}
 }
